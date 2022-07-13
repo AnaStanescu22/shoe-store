24b04a7e5cefac958ef51fcbcbef827d9e0deab2
AnaStanescu22 <stanescuanamaria22@yahoo.com>
2022-07-13 15:58:46 +0300

    Delete unused imports

--- app/src/main/java/com/example/android/shoestore/feature/InstructionFragment.kt
+++ app/src/main/java/com/example/android/shoestore/feature/InstructionFragment.kt
@@ -6,11 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.lifecycleScope
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.android.shoestore.R
 import com.example.android.shoestore.databinding.FragmentInstructionBinding
 
